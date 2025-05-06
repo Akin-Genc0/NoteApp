dac14d02b02c50b14e327f0702d1a2df03d53359
Akin-Genc0 <akingenc212@gmail.com>
2025-05-06 15:19:52 +0100

    user can enter notes and view them when view with view button

--- index.js
+++ index.js
@@ -14,15 +14,61 @@ return true;
 }
 
 
+
+
+
 saveBtn.addEventListener('click', function() {
 
+
+
+//saves notes body content
+const noteBody = document.createElement('div');
+const BodyContent = document.createTextNode(note.value);
+noteBody.appendChild(BodyContent);
+
+
+ //viewButton
+ const viewBtn = document.createElement('BUTTON');
+ viewBtn.textContent = "View Notes";
 
 //save title content
  const noteTitle = document.createElement('div');
  const titleContent = document.createTextNode(title.value);
  noteTitle.appendChild(titleContent);
+ noteTitle.appendChild(viewBtn);
 
- const currentDiv = document.getElementById("div1");
+
+//stores in div
+const currentDiv = document.getElementById("div1");
 document.body.insertBefore(noteTitle, currentDiv);
+
+//stores in div
+const noteContent = document.getElementById(".n");
+document.body.insertBefore(noteBody,  noteContent);
+
+
+//ressets inputs
+title.value = "";
+note.value = "";
+
+
+// displaying none until button pressed
+noteBody.style.display = "none";
+
+
+
+//view button
+viewBtn.addEventListener('click', function(e) { 
+    
+    
+    e.stopPropagation();
+
+    noteBody.style.display = "block";
+
+    
+
+});
+
+
 
 });
